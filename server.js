358a4520c23e32c18a50dcb2e56f7b325bf0f192
HarsimranjeetSinghMahla <readyforroast56@gmail.com>
2026-06-18 15:33:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -72,11 +72,13 @@ app.get('/api/download', (req, res) => {
 
   const args = [...baseArgs(), '-o', tmpTmpl];
 
-  iif (format === 'mp3') {
+  if (format === 'mp3') {
     args.push('-x', '--audio-format', 'mp3', '--audio-quality', '0');
 } else {
     args.push('-f', 'best');
 }
+
+args.push(safeUrl);
 
 args.push(safeUrl);
 
